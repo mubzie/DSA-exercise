a3f25b4a82940d213a3d423e75c992a7b2cc50a9
mubzie <85705074+mubzie@users.noreply.github.com>
2023-04-19 02:29:58 +0100

    started working on linked list

---
+++ first exercise/linkedList.js
@@ -0,0 +1,22 @@
+const linkedListFactory = () => {
+
+    let head = null;
+    let length = 0;
+
+    const append = (value) => 
+}
+
+const nodeFactory = (value, next) => {
+
+    value = null
+    next = null
+
+    return {
+        value,
+        nextNode
+    }
+
+}
+
+const Node = nodeFactory()
+console.log(Node.nextNode)
