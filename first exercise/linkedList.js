259eecf2d51b092ab99a12a0c25c7811a4ecbb7b
mubzie <85705074+mubzie@users.noreply.github.com>
2023-04-24 17:33:11 +0100

    working on returning tail of the list

--- first exercise/linkedList.js
+++ first exercise/linkedList.js
@@ -5,17 +5,71 @@ const nodeFactory = (value) => {
 
     return {
         value,
-        nextNode
+        next
     }
 
 }
 
 const linkedListFactory = () => {
+    let _head = null;
+    let length = 0;
 
-    let head = null;
-    let tail = null;
+    const getHead = () => {
+        return _head
+    }
 
-    const append = (value) => 
+    const append = (value) => {
+        if (_head === null) {
+            _head = nodeFactory(value)
+            length++
+            return
+        }
+
+        let current = _head
+        while (current.next !== null) {
+            current = current.next
+        }
+        current.next = nodeFactory(value)
+        length++
+    }
+
+    const prepend = (value) => {
+        let newHead = nodeFactory(value)
+        newHead.next = _head
+        _head = newHead
+        length++
+    }
+
+    const size = () => {
+        return length
+    }
+
+    const head = () => {
+        if (_head !== null) {
+            return Object.values(_head)[0]
+        }
+    }
+    
+    const tail = () => {
+        let current = _head
+        if (current.next === null) {
+            return current
+        }
+    }
+    
+    const at = (value) => {
+            return Object.values(_head)[value]
+    }
+
+    return {
+        append,
+        prepend,
+        getHead,
+        size,
+        head,
+        tail,
+        at
+    }
 }
 
 
